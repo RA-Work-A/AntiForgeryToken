6ff30d9536041159053c95a3559806c111e4a173
Randy A <randy.augustus@americo.com>
2019-09-19 13:01:52 -0500

    init and final check-in for trouble ticket to M$

---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDataProtection()
+                .SetApplicationName("iPadTesting")
+                .PersistKeysToFileSystem(SetKeys());
+            //.ProtectKeysWithCertificate("thumbprint");
+            //.SetDefaultKeyLifetime(TimeSpan.FromDays(14));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        private DirectoryInfo SetKeys()
+        {
+            var locationExists = Directory.Exists(@"D:\ipadTesting\Keys");
+            if (!locationExists)
+            {
+                Directory.CreateDirectory(@"D:\ipadTesting\Keys");
+                var di = new DirectoryInfo(@"D:\ipadTesting\Keys");
+
+                return di;
+            }
+
+            return new DirectoryInfo(@"D:\ipadTesting\Keys");
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
